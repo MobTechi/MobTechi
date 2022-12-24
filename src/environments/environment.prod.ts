326f79f4a8f9c1d5395d44ada7063e7e8d89ec24
poovarasan <poovarasan@bambeeqs-MacBook-Air-2.local>
2022-12-24 17:36:39 +0530

    v1.3: Implemented Play store and Telegram contact us

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,11 @@ export const environment = {
   email: 'support@mobtechi.com',
   social: [
     {
+      name: 'Play Store',
+      cssClass: 'google-play',
+      url: 'https://play.google.com/store/apps/dev?id=7579037844327234040',
+    },
+    {
       name: 'GitHub',
       cssClass: 'github',
       url: 'https://github.com/MobTechi',
@@ -12,6 +17,11 @@ export const environment = {
       name: 'Instagram',
       cssClass: 'instagram',
       url: 'https://www.instagram.com/mobtechi',
+    },
+    {
+      name: 'Telegram',
+      cssClass: 'telegram',
+      url: 'https://telegram.me/mobtechi',
     }
   ]
 };
